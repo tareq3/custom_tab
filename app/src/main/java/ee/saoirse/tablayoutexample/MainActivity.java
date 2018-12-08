62ca87cc12bd83502acb7eef8748adac8d990523
kuldar-daniel <kuldar.daniel@gmail.com>
2018-12-08 20:10:47 +0200

    TabLayout with rounded Tabs example

---
+++ app/src/main/java/ee/saoirse/tablayoutexample/MainActivity.java
@@ -0,0 +1,23 @@
+package ee.saoirse.tablayoutexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tabLayout = findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("First"));
+        tabLayout.addTab(tabLayout.newTab().setText("Second"));
+        tabLayout.addTab(tabLayout.newTab().setText("Third"));
+    }
+}
