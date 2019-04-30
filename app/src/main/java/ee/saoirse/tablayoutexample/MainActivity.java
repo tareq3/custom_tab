beb607b0c3017484367346aa49221b692d135be2
tareq3 <mti.tareq3@gmai.com>
2019-04-30 11:59:41 +0600

    tab fragment transition added without viewpager

--- app/src/main/java/ee/saoirse/tablayoutexample/MainActivity.java
+++ app/src/main/java/ee/saoirse/tablayoutexample/MainActivity.java
@@ -1,12 +1,24 @@
 package ee.saoirse.tablayoutexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+import ee.saoirse.tablayoutexample.fragments.TopCheckerFragment;
+import ee.saoirse.tablayoutexample.fragments.TopCollectionerFragment;
+import ee.saoirse.tablayoutexample.fragments.TopSellerFragment;
+
+public class MainActivity extends AppCompatActivity {
+    public enum FRAGMENT_TAGS {
+        Top_Seller, Top_Collectioner, Top_Checker
+    }
     private TabLayout tabLayout;
 
     @Override
@@ -15,8 +27,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tabLayout = findViewById(R.id.tabLayout);
+        getFragment(TopSellerFragment.newInstance(),FRAGMENT_TAGS.Top_Seller);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()){
+                    case 0:
+                        getFragment(TopSellerFragment.newInstance(),FRAGMENT_TAGS.Top_Seller);
+                        break;
+                    case 1:
+                        getFragment(TopCollectionerFragment.newInstance(),FRAGMENT_TAGS.Top_Collectioner);
+                        break;
+                    case 2:
+                        getFragment(TopCheckerFragment.newInstance(),FRAGMENT_TAGS.Top_Checker);
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
 
+    }
 
+    private void getFragment(Fragment fragment, FRAGMENT_TAGS fragTag) {
+      FragmentManager fragmentManager = getSupportFragmentManager();
+
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        switch (fragTag) {
+
+            case Top_Checker:
+                if (getVisibleFragment() != null && !getVisibleFragment().getTag().equalsIgnoreCase(FRAGMENT_TAGS.Top_Checker.toString()))
+
+                    fragmentTransaction.setCustomAnimations(R.anim.right_to_left, R.anim.right_out_left);
+                break;
+            case Top_Seller:
+                if (getVisibleFragment() != null && !getVisibleFragment().getTag().equalsIgnoreCase(FRAGMENT_TAGS.Top_Seller.toString()))
+
+                    fragmentTransaction.setCustomAnimations(R.anim.left_to_right, R.anim.left_out_right);
+                break;
+            case Top_Collectioner:
+                if (getVisibleFragment() != null) {
+                    if (getVisibleFragment().getTag().equalsIgnoreCase(FRAGMENT_TAGS.Top_Seller.toString()))
+                        fragmentTransaction.setCustomAnimations(R.anim.right_to_left, R.anim.right_out_left);
+                    else if (getVisibleFragment().getTag().equalsIgnoreCase(FRAGMENT_TAGS.Top_Checker.toString()))
+                        fragmentTransaction.setCustomAnimations(R.anim.left_to_right, R.anim.left_out_right);
+                }
+
+                break;
+        }
+
+        fragmentTransaction.replace(R.id.content_fragment, fragment, fragTag.toString())
+                .commit();
+
+    }
+
+    public Fragment getVisibleFragment() {
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        List<Fragment> fragments = fragmentManager.getFragments();
+        if (fragments != null) {
+            for (Fragment fragment : fragments) {
+                if (fragment != null && fragment.isVisible()) {
+
+                    return fragment;
+                }
+            }
+        }
+        return null;
     }
 }
---
+++ app/src/main/java/ee/saoirse/tablayoutexample/fragments/TopCollectionerFragment.java
@@ -0,0 +1,42 @@
+/*
+ * Created by Tareq Islam on 4/30/19 11:12 AM
+ *
+ *  Last modified 4/30/19 11:12 AM
+ */
+
+package ee.saoirse.tablayoutexample.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ee.saoirse.tablayoutexample.R;
+
+/***
+ * Created by mtita on 30,April,2019.
+ */
+public class TopCollectionerFragment extends Fragment {
+    private TopCollectionerFragment() {
+    }
+
+    public  static TopCollectionerFragment newInstance(){
+        return new TopCollectionerFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main_top_collecter_fragment, container, false);
+        return view;
+    }
+}
